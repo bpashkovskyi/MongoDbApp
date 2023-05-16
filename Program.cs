383afd277d814fc10f1a7c7b242622ed0cafd260
Bohdan Pashkovskyi <bpashkovskyi@gmail.com>
2023-05-16 09:02:56 +0300

    Added Cache

--- Program.cs
+++ Program.cs
@@ -1,6 +1,10 @@
 using MongoDbApp;
 using MongoDbApp.Repositories;
 
+using StackExchange.Redis;
+
+using System.Globalization;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -13,6 +17,24 @@ builder.Services.AddSwaggerGen();
 builder.Services.Configure<MongoDbSettings>(mongoDbSettings => builder.Configuration.GetSection("MongoDbSettings").Bind(mongoDbSettings));
 
 builder.Services.AddScoped<IUserRepository, UserRepository>();
+
+//Configure other services up here
+var multiplexer = ConnectionMultiplexer.Connect("bpashkovskyi.redis.cache.windows.net:6380,password=FHfJ8zwNItRqK3ksuHHyEh4ufgZ6Z2lqAAzCaKXIujI=,ssl=True,abortConnect=False");
+builder.Services.AddSingleton<IConnectionMultiplexer>(multiplexer);
+
+builder.Services.AddStackExchangeRedisCache(
+    redisCacheOptions =>
+    {
+        redisCacheOptions.ConfigurationOptions = new ConfigurationOptions
+        {
+            AllowAdmin = true,
+            DefaultDatabase = 0,
+            Ssl = true,
+            Password = "FHfJ8zwNItRqK3ksuHHyEh4ufgZ6Z2lqAAzCaKXIujI=",
+            EndPoints = { { "bpashkovskyi.redis.cache.windows.net", 6380 } }
+        };
+    });
+
 
 builder.Services.AddAutoMapper(typeof(UserMapperProfile).Assembly);
 
--- Repositories/UserRepository.cs
+++ Repositories/UserRepository.cs
@@ -6,34 +6,49 @@ using MongoDB.Bson;
 using MongoDB.Driver;
 
 using Model;
+using Microsoft.Extensions.Caching.Distributed;
+using System.Text.Json;
 
 public class UserRepository : IUserRepository
 {
     private readonly IMongoCollection<User> _users;
+    private readonly IDistributedCache _distributedCache;
 
-    public UserRepository(IOptions<MongoDbSettings> settingsOptions)
+    public UserRepository(
+        IOptions<MongoDbSettings> settingsOptions,
+        IDistributedCache distributedCache)
     {
+        _distributedCache = distributedCache;
         var settings = settingsOptions.Value;
 
         var client = new MongoClient(settings.ConnectionString);
         var userDatabase = client.GetDatabase(settings.UserDatabaseName);
 
-        this._users = userDatabase.GetCollection<User>("Users");
+        _users = userDatabase.GetCollection<User>("Users");
     }
 
     public async Task<List<User>> GetAllUsersAsync()
     {
-        return await this._users.AsQueryable().ToListAsync();
+        return await _users.AsQueryable().ToListAsync();
     }
 
     public async Task<User> GetUserAsync(ObjectId id)
     {
-        return await this._users.Find(u => u.Id == id).FirstOrDefaultAsync();
+        var user = await GetUserFromCacheAsync(id);
+        if (user is not null)
+        {
+            return user;
+        }
+
+        user = await GetUserFromDatabaseAsync(id);
+        await SetUserToCacheAsync(user);
+
+        return user;
     }
 
     public async Task AddUserAsync(User user)
     {
-        await this._users.InsertOneAsync(user);
+        await _users.InsertOneAsync(user);
     }
 
     public async Task UpdateUserAsync(User user)
@@ -46,7 +61,9 @@ public class UserRepository : IUserRepository
             .Set(u => u.Email, user.Email)
             .Set(u => u.Role, user.Role);
 
-        await this._users.UpdateOneAsync(filter, update);
+        await _users.UpdateOneAsync(filter, update);
+
+        await ClearUserFromCacheAsync(user.Id);
     }
 
     public async Task UpdateUserNameAsync(ObjectId id, string name)
@@ -57,7 +74,9 @@ public class UserRepository : IUserRepository
         var update = Builders<User>.Update
             .Set(u => u.Name, name);
 
-        await this._users.UpdateOneAsync(filter, update);
+        await _users.UpdateOneAsync(filter, update);
+
+        await ClearUserFromCacheAsync(id);
     }
 
     public async Task UpdateUserRoleAsync(ObjectId id, string role)
@@ -68,7 +87,9 @@ public class UserRepository : IUserRepository
         var update = Builders<User>.Update
             .Set(u => u.Role, role);
 
-        await this._users.UpdateOneAsync(filter, update);
+        await _users.UpdateOneAsync(filter, update);
+
+        await ClearUserFromCacheAsync(id);
     }
 
     public async Task DeleteUserAsync(ObjectId id)
@@ -76,6 +97,43 @@ public class UserRepository : IUserRepository
         var filter = Builders<User>.Filter
             .Eq(u => u.Id, id);
 
-        await this._users.DeleteOneAsync(filter);
+        await _users.DeleteOneAsync(filter);
+
+        await ClearUserFromCacheAsync(id);
+    }
+
+    private async Task<User> GetUserFromDatabaseAsync(ObjectId id)
+    {
+        return await _users.Find(u => u.Id == id).FirstOrDefaultAsync();
+    }
+
+    private async Task<User> GetUserFromCacheAsync(ObjectId id)
+    {
+        var cacheKey = id.ToString();
+
+        var userAsString = await _distributedCache.GetStringAsync(cacheKey);
+        if (userAsString is null)
+        {
+            return null;
+        }
+
+        return JsonSerializer.Deserialize<User>(userAsString);
+    }
+
+    private async Task SetUserToCacheAsync(User user)
+    {
+        var cacheKey = user.Id.ToString();
+
+        var userAsString = JsonSerializer.Serialize(user);
+
+        var distributedCacheEntryOptions = new DistributedCacheEntryOptions { SlidingExpiration = TimeSpan.FromMinutes(1) };
+        await _distributedCache.SetStringAsync(cacheKey, userAsString, distributedCacheEntryOptions);
+    }
+
+    private async Task ClearUserFromCacheAsync(ObjectId id)
+    {
+        var cacheKey = id.ToString();
+
+        await _distributedCache.RemoveAsync(cacheKey);
     }
 }
